1da090ec4ecf52cd7453a144ef803ea427529c03
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-02-26 21:54:32 +0500

    Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import React,{useState} from 'react';
+import React from 'react';
 import './App.css';
-import TodoList from './components/TodoList';
+import NewTodoList from'./components/NewTodoList'
 function App() {
-  // const [todos, setTodos] = useState([
-  //   { id: 1, text: 'Buy groceries', completed: false },
-  //   { id: 2, text: 'Walk the dog', completed: true },
-  //   { id: 3, text: 'Clean the house', completed: false },
-  // ]);
-
-  // const handleCheck = (id) => {
-  //   setTodos((prevTodos) =>
-  //     prevTodos.map((todo) =>
-  //       todo.id === id ? { ...todo, completed: !todo.completed } : todo
-  //     )
-  //   );  
-  // };
-
-  // const handleDelete = (id) => {
-  //   setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-  // };
   return (
     <div className="App">
-      <TodoList />
-      {/* <div>
-        <h1>Todo List</h1>
-        {todos.map((todo) => (
-          <div key={todo.id}>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => handleCheck(todo.id)}
-            />
-            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.text}</span>
-            <button onClick={() => handleDelete(todo.id)}>Delete</button>
-          </div>
-        ))}
-      </div> */}
+      <NewTodoList/>
+
     </div>
   );
 }
